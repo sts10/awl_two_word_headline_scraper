1ea68e406bbcc7ac05c39668d9fc615fc4d8a00d
sts10 <sschlinkert@gmail.com>
2016-05-07 11:07:56 -0400

    switch node packages, exclude song names

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-var http = require("http");
+var http = require('http');
 var cheerio = require('cheerio');
 var baseUrl = "http://www.theawl.com/page/";
+var csv = require('ya-csv'),  
+fs = require('fs');
 
 var allPostTitles = [];
+
+var writer = csv.createCsvStreamWriter(fs.createWriteStream('test_headlines.csv', {'flags': 'a'}));  
 
 function getPage(pageNum){
   var request = http.get(baseUrl+pageNum, function(response){
@@ -14,15 +18,23 @@ function getPage(pageNum){
     response.on('end', function(){
       var page = cheerio.load(body);
       var posts = page("div.reverse-chron__post h2 a");
-      // console.log(posts[1]['children'][0]['data']);
+      console.log("childen:");
+      console.log(posts[1]["attribs"]['href']);
 
       var post_titles = [];
       var i = 0;
-      while(i< posts.length){
-        post_titles.push(posts[i]['children'][0]['data']);
+      while(i < posts.length){
+        var postTitle = posts[i]['children'][0]['data'];
+        if (postTitle.split(" ").length == 2 && postTitle.split(' ')[1].indexOf('"') == -1) {
+          var postURL = posts[i]['attribs']['href'];
+          var postInfo = [postTitle, postURL];
+
+          writer.writeRecord(postInfo);
+          post_titles.push(postTitle);
+        }
         i = i + 1;
       }
-      console.dir(post_titles);
+      // console.dir(post_titles);
       allPostTitles.concat(post_titles);
     });
 
@@ -30,7 +42,7 @@ function getPage(pageNum){
 }
 
 var i = 1;
-var totalPagesToScrape = 10;
+var totalPagesToScrape = 2000;
 while (i < totalPagesToScrape){
   var thisPagePostTitles = getPage(i);
   i = i + 1;
