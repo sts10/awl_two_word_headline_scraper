387ff5a6d185507112786f124bc8df4edda88870
sts10 <sschlinkert@gmail.com>
2016-05-08 20:42:13 -0400

    removes an uneccessary array from app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var csv = require('ya-csv'),
 fs = require('fs');
 
 var baseUrl = "http://www.theawl.com/page/";
-var allPostTitles = [];
 
 var writer = csv.createCsvStreamWriter(fs.createWriteStream('test_headlines.csv', {'flags': 'a'}));  
 
@@ -19,7 +18,6 @@ function getPage(pageNum){
       var page = cheerio.load(body);
       var posts = page("div.reverse-chron__post h2 a");
 
-      var post_titles = [];
       var i = 0;
       while(i < posts.length){
         if (posts[i]['children'][0] && posts[i]['children'][0]['data']){
@@ -27,16 +25,12 @@ function getPage(pageNum){
           if (postTitle.split(" ").length == 2 && postTitle.split(' ')[1].indexOf('"') == -1) {
             var postURL = posts[i]['attribs']['href'];
             var postInfo = [postTitle, postURL];
-            console.log(postInfo);
 
             writer.writeRecord(postInfo);
-            post_titles.push(postTitle);
           }
         }
         i = i + 1;
       }
-      // console.dir(post_titles);
-      allPostTitles.concat(post_titles);
     });
 
   });
